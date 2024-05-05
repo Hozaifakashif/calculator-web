5c1dff092f9d54d5893745db91085776c21ac8bb
hozaifakashif <kashifshaheeer@gmail.com>
2024-05-05 21:22:13 +0500

    full website completed

---
+++ app.js
@@ -0,0 +1,25 @@
+
+  function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearLastEntry() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculate() {
+    try {
+      const result = eval(document.getElementById('display').value);
+      document.getElementById('display').value = result;
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
+
+
+
